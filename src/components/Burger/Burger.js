1ab6103b49f4afbeeb383987fbd35d42193b569e
Remi Kristelijn <rkristelijn@gmail.com>
2018-09-24 14:07:53 +0200

    added dynamic rendering using obscure nested functions

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    // Object.keys() returns an array with the keys of the object
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => { // map executes a function for every element in an array
+            //Array returns an array with an amount of items provided as argument
+            return [...Array(props.ingredients[igKey])].map((_, i) => { 
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
