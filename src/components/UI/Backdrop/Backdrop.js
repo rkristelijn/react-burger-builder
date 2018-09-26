a3f919ea6f2c3370eef0cb1da54fb662ab326e28
Remi Kristelijn <rkristelijn@gmail.com>
2018-09-26 14:21:48 +0200

    added logo in Toolbar

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -4,7 +4,7 @@ import classes from './Backdrop.css';
 const backdrop = (props) => (
     props.show ? <div
         className={classes.Backdrop}
-        onClick={props.click}
+        onClick={props.clicked}
     >{props.children}</div> : null
 );
 
