d338a7e8d07976fc49cef4a27b82f9da50aebd86
Remi Kristelijn <rkristelijn@gmail.com>
2018-09-27 08:54:14 +0200

    changed ordersummary from function to class to identify when it is rendered by adding a hook

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(
+class OrderSummary extends Component {
+    ingredientSummary = Object.keys(this.props.ingredients).map(
         igKey => {
             return (<li key={igKey}>
                 <span
                     style={{ textTransform: 'capitalize' }}>
-                    {igKey}:{props.ingredients[igKey]}
+                    {igKey}:{this.props.ingredients[igKey]}
                 </span>
             </li >)
         }
     );
-    return (
+    render = () =>
+     (
         <Aux>
             <h1>Your Order</h1>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
-                {ingredientSummary}
+                {this.ingredientSummary}
             </ul>
-            <p><strong>Total Price: $ {props.price}</strong></p>
+            <p><strong>Total Price: $ {this.props.price}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType='Danger' clicked={props.cancel}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.continue}>CONTINUE</Button>
+            <Button btnType='Danger' clicked={this.props.cancel}>CANCEL</Button>
+            <Button btnType='Success' clicked={this.props.continue}>CONTINUE</Button>
         </Aux>
     )
 }
 
-export default orderSummary;
+export default OrderSummary;
